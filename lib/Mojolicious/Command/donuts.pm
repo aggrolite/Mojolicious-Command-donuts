e5265900e38a822f86e87c8275d657ac96cbee81
Curtis Brandt <curtis@cpan.org>
2015-06-14 12:09:37 -0700

    0.02

--- lib/Mojolicious/Command/donuts.pm
+++ lib/Mojolicious/Command/donuts.pm
@@ -5,7 +5,7 @@ use Mojo::JSON qw(encode_json);
 use Mojo::Util qw(dumper);
 use Getopt::Long qw(GetOptionsFromArray);
 
-our $VERSION = 0.01;
+our $VERSION = 0.02;
 
 has description => 'Find fresh donuts near you!';
 
